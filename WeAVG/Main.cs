706ba4681387d412cf13dc9e1d2c3acd15428e4c
AleksanderGalkin <agalkinov@mail.ru>
2015-04-21 18:08:19 +0700

    Рабочая версия SQL CLR агрегата

---
+++ WeAVG/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+namespace WeAvgAssembly
+{
+
+     
+    [Serializable]
+    [Microsoft.SqlServer.Server.SqlUserDefinedAggregate(Format.Native, IsInvariantToNulls = true)]
+    public struct WeAVG
+    {
+        private SqlDouble sum_;
+        private SqlDouble count_;
+
+        public void Init()
+        {
+            sum_ = 0;
+            count_ = 0;
+        }
+
+        public void Accumulate(SqlDouble Vol, SqlDouble Ccp)
+        {
+            if (!(Vol.IsNull || Ccp.IsNull))
+            {
+                sum_ = sum_ + (Vol * Ccp);
+                count_ = count_ + Vol;
+            }
+        }
+
+        public void Merge(WeAVG Group)
+        {
+            sum_ = sum_ + Group.sum_;
+            count_ = count_ + Group.count_;
+        }
+
+        public SqlDouble Terminate()
+        {
+
+            return count_ == 0 ? SqlDouble.Null : sum_ / count_;
+
+        }
+     
+    }
+   
+}
